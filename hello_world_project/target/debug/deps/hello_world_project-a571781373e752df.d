6ccdd2e25074d8db2f65da7f5ad1ac79a9848672
Kiều Phong BB'm Real <kieuphong2840@gmail.com>
2025-09-25 15:39:07 +0700

    create hello world project

---
+++ hello_world_project/target/debug/deps/hello_world_project-a571781373e752df.d
@@ -0,0 +1,5 @@
+/Users/vinhngo/rust-on-my-way/hello_world_project/target/debug/deps/hello_world_project-a571781373e752df: src/main.rs
+
+/Users/vinhngo/rust-on-my-way/hello_world_project/target/debug/deps/hello_world_project-a571781373e752df.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world_project/target/debug/hello_world_project.d
@@ -0,0 +1,1 @@
+/Users/vinhngo/rust-on-my-way/hello_world_project/target/debug/hello_world_project: /Users/vinhngo/rust-on-my-way/hello_world_project/src/main.rs
